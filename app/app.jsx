d2a71eef8ea55750c3953bb0d52bc8890e3a34be
Moustafa Badawy <moustafa.badawym@gmail.com>
2015-07-22 10:42:18 +0200

    Basic page.js routing

--- app/app.jsx
+++ app/app.jsx
@@ -1,4 +1,32 @@
-var React = require('react');
+var React   = require('react');
 var Counter = require('./components/counter');
+var page    = require('page');
+
+// Routes
+// the "notfound" implements a catch-all
+// with page('*', notfound). Here we have
+// no catch-all, so page.js will redirect
+// to the location of paths which do not
+// match any of the following routes
+//
+// page.base('/basic');
+
+page('/', index);
+page('/about', about);
+page('/contact', contact);
+page('/contact/:contactName', contact);
+page();
+
+function index() {
+  alert('index');
+}
+
+function about() {
+  alert('about');
+}
+
+function contact(ctx) {
+  alert('contact');
+}
 
 React.render(<Counter count={0}/>, document.getElementById('mount-point'));
